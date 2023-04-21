7ca5b2e8546e8fcfc991303f8ef67c81d90395b2
AnthonySar <anthony.sarapata@gmail.com>
2023-04-21 17:09:10 +0200

    :wrench: Cypress : Test du Create/Delete de book en HP

--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -1,6 +1,31 @@
 /* eslint-disable no-undef */
-describe("Page d'accueil", () => {
-  it("Chargement terminé", () => {
+describe("Page d'accueil, test des inputs, ajout d'un livre, suppresion d'un livre et de tous les livres", () => {
+  beforeEach(() => {
     cy.visit("/");
+  });
+
+  // Fct permettant d'ajouter un livre, on repéte l'opération pour simuler les clicks (delete & deleteAll)
+  function addNewBook() {
+    cy.get('input[placeholder="Titre du livre"]').type("My New Book");
+    cy.get('input[placeholder="Auteur du livre"]').type("Jane Doe");
+    cy.get("[data-add-book]").click();
+    cy.contains("My New Book");
+    cy.contains("Jane Doe");
+  }
+
+  it("should add a new book", () => {
+    addNewBook();
+  });
+
+  it("should delete a book", () => {
+    addNewBook();
+    cy.get("[data-delete-book]").click();
+    cy.contains("Il n'y a aucun livre dans la collection");
+  });
+
+  it("should delete all books", () => {
+    addNewBook();
+    cy.get("[data-delete-allbook]").click();
+    cy.contains("Il n'y a aucun livre dans la collection");
   });
 });
--- src/containers/AddBooks.js
+++ src/containers/AddBooks.js
@@ -1,15 +1,19 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux'; 
-import { addBook, deleteAllBook, deleteBook } from '../redux/actions/actionBooks';
-import { MdOutlineDeleteForever } from 'react-icons/md'
-import FlipMove from 'react-flip-move';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  addBook,
+  deleteAllBook,
+  deleteBook,
+} from "../redux/actions/actionBooks";
+import { MdOutlineDeleteForever } from "react-icons/md";
+import FlipMove from "react-flip-move";
 
 const AddBooks = () => {
   const initialState = {
-    title: '',
-    author: ''
+    title: "",
+    author: "",
   };
-  
+
   const [newData, setNewData] = useState(initialState);
 
   // On gére notre store avec les Hooks Redux
@@ -24,93 +28,116 @@ const AddBooks = () => {
 
     // On réinitialise la data à 0
     setNewData(initialState);
-  }
+  };
 
   const handleDelete = (id) => {
     // L'action est dans le reducerBooks : case DELETE_BOOKS
     // On retire la data en fonction de l'id if(!==)
-    dispatch(deleteBook(id))
-  }
+    dispatch(deleteBook(id));
+  };
 
   const handleAllDelete = () => {
     // On vide l'array tout simplement depuis notre reducerBooks
-    dispatch(deleteAllBook())
-  }
+    dispatch(deleteAllBook());
+  };
 
   // On procéde au traitement de la data à afficher avec la méthode map()
-  const displayData = libraryData.length > 0 ? libraryData.map((data) => {
-    return (
-      <FlipMove>
-        <li key={data.id} className='list-group-item list-group-item-light d-flex justify-content-between align-items-center'>
-          <span className='w-50'>{ data.title }</span>
-          <span className='w-50'>{ data.author }</span>
-          <span 
-            className='btn btn-danger'
-            onClick={() => handleDelete(data.id)}><MdOutlineDeleteForever /></span>
-        </li>
-      </FlipMove>
-    )
-  }) : <p className='text-center'>Il n'y a aucun livre dans la collection</p>;
+  const displayData =
+    libraryData.length > 0 ? (
+      libraryData.map((data) => {
+        return (
+          <FlipMove>
+            <li
+              key={data.id}
+              className="list-group-item list-group-item-light d-flex justify-content-between align-items-center"
+            >
+              <span className="w-50">{data.title}</span>
+              <span className="w-50">{data.author}</span>
+              <span
+                data-delete-book
+                className="btn btn-danger"
+                onClick={() => handleDelete(data.id)}
+              >
+                <MdOutlineDeleteForever />
+              </span>
+            </li>
+          </FlipMove>
+        );
+      })
+    ) : (
+      <p className="text-center">Il n'y a aucun livre dans la collection</p>
+    );
 
-  const displayBtn = libraryData.length > 0 &&
-    <div className='d-flex justify-content-center'>
-      <button 
-        className='btn btn-danger mt-4 mb-3'
-        onClick={() => handleAllDelete()}>Effacer la collection</button>
-    </div>;
+  const displayBtn = libraryData.length > 0 && (
+    <div className="d-flex justify-content-center">
+      <button
+        data-delete-allbook
+        className="btn btn-danger mt-4 mb-3"
+        onClick={() => handleAllDelete()}
+      >
+        Effacer la collection
+      </button>
+    </div>
+  );
 
   return (
-    <main role='main'>
-      <div className='d-flex rounded'>
-        <div className='container text-center'>
-          <h2 className='display-4'>Collections</h2>
+    <main role="main">
+      <div className="d-flex rounded">
+        <div className="container text-center">
+          <h2 className="display-4">Collections</h2>
           <p>Ajouter un livre à votre collections</p>
 
-          <form className='justify-content-center' onSubmit={handleSubmit}>
-            <div className='row'>
-              <div className='col'>
+          <form className="justify-content-center" onSubmit={handleSubmit}>
+            <div className="row">
+              <div className="col">
                 <input
-                  type='text'
-                  className='form-control'
-                  placeholder='Titre du livre'
-                  required 
+                  type="text"
+                  className="form-control"
+                  placeholder="Titre du livre"
+                  required
                   value={newData.title}
-                  onChange={(e) => {setNewData({...newData, title: e.target.value})}}
+                  onChange={(e) => {
+                    setNewData({ ...newData, title: e.target.value });
+                  }}
                 />
               </div>
 
-              <div className='col'>
+              <div className="col">
                 <input
-                  type='text'
-                  className='form-control ml-3'
-                  placeholder='Auteur du livre'
-                  required 
+                  type="text"
+                  className="form-control ml-3"
+                  placeholder="Auteur du livre"
+                  required
                   value={newData.author}
-                  onChange={(e) => {setNewData({...newData, author: e.target.value})}}
+                  onChange={(e) => {
+                    setNewData({ ...newData, author: e.target.value });
+                  }}
                 />
               </div>
 
-              <div className='col'>
-                <button className='btn btn-outline-secondary ml-3'>Ajouter un livre</button>
+              <div className="col">
+                <button
+                  data-add-book
+                  className="btn btn-outline-secondary ml-3"
+                >
+                  Ajouter un livre
+                </button>
               </div>
             </div>
           </form>
         </div>
       </div>
 
-      <div className='container mt-4' style={{minHeight: '200px'}}>
-
-        <div className='row'>
-          <div className='col-md-12'>
-            <ul className='list-group'>
-              { displayData }
-            </ul>
+      <div className="container mt-4" style={{ minHeight: "200px" }}>
+        <div className="row">
+          <div className="col-md-12">
+            <ul className="list-group">{displayData}</ul>
           </div>
-            { displayBtn }
+          {displayBtn}
         </div>
       </div>
     </main>
-  )
-}
+  );
+};
 
 export default AddBooks;
